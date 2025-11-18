b85a222f4054931b82f8fa5c18fc648b633cb4bf
Arnold Ayiku Kubi <kubiarnold25@gmail.com>
2025-11-18 10:15:16 +0100

    added player

---
+++ src/main/java/nl/saxion/game/Player2.java
@@ -0,0 +1,15 @@
+package nl.saxion.game;
+
+public class Player2 {
+
+    String name;
+
+    public Player2(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello, my name is " + name);
+    }
+}
+
